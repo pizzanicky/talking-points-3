6d220021bd4d7a292017b28c06a440af2ce779bd
jooh55 <jooh55@ebdd6bc0-0583-11df-8138-214a9d281a8c>
2010-08-03 17:01:37 +0000

    WPS by Hyeon

--- WozTestAndGestureTutorial/src/talkingpoints/guoer/ListComparer.java
+++ WozTestAndGestureTutorial/src/talkingpoints/guoer/ListComparer.java
@@ -6,6 +6,7 @@ import java.util.List;
 //to see if the newlist contains new POI, if yes, return a list of new POI
 public class ListComparer {
 	ArrayList<String> newItems;
+	List<String> newItems2; 	// HY. for WPS 
 	boolean hasNew = false;
 
 	public ListComparer(ArrayList<String> newList, ArrayList<String> oldList) {
@@ -25,6 +26,30 @@ public class ListComparer {
 				if (j == oldList.size() - 1)
 					hasNew = true;
  			}
+			if (hasNew) {
+				newItems.add(newList.get(i));
+				hasNew = false;
+			}
+
+		}
+	}
+	
+	public ListComparer(List<String> newList, List<String> oldList) {
+		newItems2 = new ArrayList<String>();
+		for (int i = 0; i < newList.size(); i++) {
+			if (oldList.size() == 0) {
+				newItems2 = newList;
+				break;
+			}
+			for (int j = 0; j < oldList.size(); j++) {
+				if (oldList.get(j).substring(oldList.get(j).length() - 17)
+						.equals(
+								newList.get(i).substring(
+										newList.get(i).length() - 17)))
+					break;
+				if (j == oldList.size() - 1)
+					hasNew = true;
+			}
 			if (hasNew) {
 				newItems.add(newList.get(i));
 				hasNew = false;
